1016f4d14abb4f6c470ec7a88e8baf20a0742527
Anuj Kumar <anujkumarcoder@gmail.com>
2024-11-28 01:05:35 +0530

    Corecting path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Player from './components/Player.jsx';
-import TimerChallenge from './components/timerChallenge.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx'
 
 function App() {
   
