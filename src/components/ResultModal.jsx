a8e5a59bcf64ff09b29ee76c9a8896233380005c
Anuj Kumar <anujkumarcoder@gmail.com>
2024-11-27 22:20:09 +0530

    Adding code for basic game structure

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,14 @@
+export default function ResultModal({result , targetTime}) {
+    return(
+        <dialog className="result-modal">
+            <h2>
+                You {result}
+            </h2>
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>you stopped the timer with <strong>X secondsd left.</strong> </p>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>
+    )
+}
